8ff5627bd9de99564f7e51057760205d8459443c
Zulfahmi <zulfahmi.fahmio@gmail.com>
2025-09-03 14:12:55 +0700

    update

--- android/app/build.gradle.kts
+++ android/app/build.gradle.kts
@@ -1,12 +1,11 @@
 plugins {
     id("com.android.application")
     id("kotlin-android")
-    // The Flutter Gradle Plugin must be applied after the Android and Kotlin Gradle plugins.
     id("dev.flutter.flutter-gradle-plugin")
 }
 
 android {
-    namespace = "com.odanfm.radio"
+    namespace = "com.example.radio_odan_app"
     compileSdk = flutter.compileSdkVersion
      ndkVersion = "27.0.12077973"
 
@@ -21,10 +20,10 @@ android {
 
     defaultConfig {
         // TODO: Specify your own unique Application ID (https://developer.android.com/studio/build/application-id.html).
-        applicationId = "com.odanfm.radio"
+        applicationId = "com.example.radio_odan_app"
         // You can update the following values to match your application needs.
         // For more information, see: https://flutter.dev/to/review-gradle-config.
-        minSdk = flutter.minSdkVersion
+        minSdk = 23
         targetSdk = flutter.targetSdkVersion
         versionCode = flutter.versionCode
         versionName = flutter.versionName
